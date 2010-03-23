0bfcdd93d2c29f07e16f0f11cf49b7dd9a6a9581
Anthony Starks <ajstarks@gmail.com>
2010-03-22 22:28:49 -0400

    sync up

--- flower.go
+++ flower.go
@@ -11,9 +11,16 @@ import (
   "flag"
 )
 
-var niter = flag.Int("n", 200, "number of iterations")
-var width = flag.Int("w", 500, "width")
-var height = flag.Int("h", 500, "height")
+var (
+ niter = flag.Int("n", 200, "number of iterations")
+ width = flag.Int("w", 500, "width")
+ height = flag.Int("h", 500, "height")
+ thickness = flag.Int("t", 10, "max thinkness")
+ np = flag.Int("p", 15, "max number of points")
+ psize = flag.Int("s", 30, "max length of petals")
+ opacity = flag.Int("o", 50, "maximum opacity (10-100)")
+)
+
 
 func radial(xp int, yp int, n int, l int, style ...string) {
 	var x, y, r, t, limit float64
@@ -43,10 +50,10 @@ func randrad(w int, h int, n int) {
 		r = rand.Intn(255)
 		g = rand.Intn(255)
 		b = rand.Intn(255)
-		o = random(10, 50)
-		s = random(10, 60)
-		t = random(2, 10)
-		p = random(10, 15)
+		o = random(10, *opacity)
+		s = random(10, *psize)
+		t = random(2, *thickness)
+		p = random(10, *np)
 		radial(x, y, p, s,
 			fmt.Sprintf("stroke:rgb(%d,%d,%d); stroke-opacity:%.2f; stroke-width:%d",
 				r, g, b, float64(o)/100.0, t))
