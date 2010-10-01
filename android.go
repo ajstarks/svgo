767fdf8c3d58c23ddfa7ca896976d5cad5f49a76
Anthony Starks <ajstarks@gmail.com>
2010-09-30 21:40:20 -0400

    ... syntax, canvas

--- android.go
+++ android.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	svglib "svg"
+	"svg"
 	"os"
 	"fmt"
 )
@@ -9,43 +9,43 @@ import (
 var (
 	width  = 500
 	height = 500
-	svg    = svglib.New(os.Stdout)
+	canvas    = svg.New(os.Stdout)
 )
 
 const androidcolor = "rgb(164,198,57)"
 
-func background(v int) { svg.Rect(0, 0, width, height, svg.RGB(v, v, v)) }
+func background(v int) { canvas.Rect(0, 0, width, height, canvas.RGB(v, v, v)) }
 
 func android(x, y int, fill string, opacity float) {
 	var linestyle = []string{`stroke="` + fill + `"`, `stroke-linecap="round"`, `stroke-width="5"`}
 	globalstyle := fmt.Sprintf("fill:%s;opacity:%.2f", fill, opacity)
-	svg.Gstyle(globalstyle)
-	svg.Arc(x+30, y+70, 35, 35, 0, false, true, x+130, y+70)                     // head
-	svg.Line(x+60, y+25, x+50, y+10, linestyle[0], linestyle[1], linestyle[2])   // left antenna
-	svg.Line(x+100, y+25, x+110, y+10, linestyle[0], linestyle[1], linestyle[2]) // right antenna
-	svg.Circle(x+60, y+45, 5, "fill:white")                                      // left eye
-	svg.Circle(x+100, y+45, 5, `fill="white"`)                                   // right eye
-	svg.Roundrect(x+30, y+75, 100, 90, 10, 10)                                   // body
-	svg.Rect(x+30, y+75, 100, 80)
-	svg.Roundrect(x+5, y+80, 20, 70, 10, 10)   // left arm
-	svg.Roundrect(x+135, y+80, 20, 70, 10, 10) // right arm
-	svg.Roundrect(x+50, y+150, 20, 50, 10, 10) // left leg
-	svg.Roundrect(x+90, y+150, 20, 50, 10, 10) // right leg
-	svg.Gend()
+	canvas.Gstyle(globalstyle)
+	canvas.Arc(x+30, y+70, 35, 35, 0, false, true, x+130, y+70)                     // head
+	canvas.Line(x+60, y+25, x+50, y+10, linestyle[0], linestyle[1], linestyle[2])   // left antenna
+	canvas.Line(x+100, y+25, x+110, y+10, linestyle[0], linestyle[1], linestyle[2]) // right antenna
+	canvas.Circle(x+60, y+45, 5, "fill:white")                                      // left eye
+	canvas.Circle(x+100, y+45, 5, `fill="white"`)                                   // right eye
+	canvas.Roundrect(x+30, y+75, 100, 90, 10, 10)                                   // body
+	canvas.Rect(x+30, y+75, 100, 80)
+	canvas.Roundrect(x+5, y+80, 20, 70, 10, 10)   // left arm
+	canvas.Roundrect(x+135, y+80, 20, 70, 10, 10) // right arm
+	canvas.Roundrect(x+50, y+150, 20, 50, 10, 10) // left leg
+	canvas.Roundrect(x+90, y+150, 20, 50, 10, 10) // right leg
+	canvas.Gend()
 }
 
 func main() {
-	svg.Start(width, height)
-	svg.Title("Android")
+	canvas.Start(width, height)
+	canvas.Title("Android")
 	background(255)
 
 	android(100, 100, androidcolor, 1.0)
-	svg.Gtransform("scale(3.0,3.0)")
+	canvas.Gtransform("scale(3.0,3.0)")
 	android(50, 50, "gray", 0.5)
-	svg.Gend()
+	canvas.Gend()
 
-	svg.Gtransform("scale(0.5,0.5)")
+	canvas.Gtransform("scale(0.5,0.5)")
 	android(100, 100, "red", 1.0)
-	svg.Gend()
-	svg.End()
+	canvas.Gend()
+	canvas.End()
 }
