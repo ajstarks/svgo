39309edae9a379741f9acccdcad474048105d2bd
Anthony Starks <ajstarks@gmail.com>
2010-10-27 22:00:24 -0400

    new composite literal syntax

--- gradient.go
+++ gradient.go
@@ -9,27 +9,28 @@ import (
 func main() {
 	width := 500
 	height := 500
-	oc1 := svg.Offcolor{0, "rgb(255,255,0)", 1.0}
-	oc2 := svg.Offcolor{100, "rgb(255,0,0)", .5}
-	oc3 := svg.Offcolor{0, "rgb(200,200,200)", 0.0}
-	oc4 := svg.Offcolor{100, "rgb(0,0,255)", 1.0}
-	oc5 := svg.Offcolor{10, "#00cc00", 1}
-	oc6 := svg.Offcolor{30, "#006600", 1}
-	oc7 := svg.Offcolor{70, "#cc0000", 1}
-	oc8 := svg.Offcolor{90, "#000099", 1}
-	
-	oc9 := svg.Offcolor{1, "powderblue", 1}
-	oc10 := svg.Offcolor{10, "lightskyblue", 1}
-	oc11 := svg.Offcolor{100, "darkblue", 1}
 
-	lg := []svg.Offcolor{oc1, oc2, oc3, oc4}
-	rg := []svg.Offcolor{oc9, oc10, oc11}
-	rainbow := []svg.Offcolor{oc5, oc6, oc7, oc8}
+	lg := []svg.Offcolor{
+		{0, "rgb(255,255,0)", 1.0},
+		{100, "rgb(255,0,0)", .5},
+		{0, "rgb(200,200,200)", 0.0},
+		{100, "rgb(0,0,255)", 1.0}}
+
+	rainbow := []svg.Offcolor{
+		{10, "#00cc00", 1},
+		{30, "#006600", 1},
+		{70, "#cc0000", 1},
+		{90, "#000099", 1}}
+
+	rg := []svg.Offcolor{
+		{1, "powderblue", 1},
+		{10, "lightskyblue", 1},
+		{100, "darkblue", 1}}
 
 	g := svg.New(os.Stdout)
 	g.Start(width, height)
 	g.Title("Gradients")
-	g.Rect(0,0,width,height,"fill:white")
+	g.Rect(0, 0, width, height, "fill:white")
 	g.Def()
 	g.LinearGradient("h", 0, 100, 0, 0, lg)
 	g.LinearGradient("v", 0, 0, 100, 0, lg)
