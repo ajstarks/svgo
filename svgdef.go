481195b05f4c4b0d290038b312616ff0e3e1e47b
Anthony Starks <ajstarks@gmail.com>
2011-04-08 01:04:10 -0400

    refactor svgdef

--- svgdef.go
+++ svgdef.go
@@ -3,25 +3,25 @@
 package main
 
 import (
-	"svg"
+	"github.com/ajstarks/svgo"
 	"os"
 )
 
 const (
-	textsize    = 15
+	textsize    = 20
 	coordsize   = 4
 	objstyle    = "fill:none; stroke-width:2; stroke:rgb(127,0,0)"
 	fobjstyle   = "fill:rgb(127,0,0);fill-opacity:0.25"
 	legendstyle = "fill:gray; text-anchor:middle"
-	titlestyle  = "fill:black; text-anchor:middle;font-size:16px"
+	titlestyle  = "fill:black; text-anchor:middle;font-size:20px"
 	linestyle   = "stroke:black; stroke-width:1"
-	gtextstyle  = "font-family:Calibri; text-anchor:middle; font-size:14px"
+	gtextstyle  = "font-family:Calibri; text-anchor:middle; font-size:18px"
 	coordstring = "x, y"
 )
 
 var (
 	canvas   = svg.New(os.Stdout)
-	grayfill = canvas.RGB(200, 200, 200)
+	grayfill = canvas.RGB(220, 220, 220)
 	oc1      = svg.Offcolor{0, "red", 1.0}
 	oc2      = svg.Offcolor{50, "gray", 1.0}
 	oc3      = svg.Offcolor{100, "black", 1.0}
@@ -33,53 +33,54 @@ func defcoordstr(x int, y int, s string) {
 	canvas.Text(x, y-textsize, s, legendstyle)
 }
 
-func defcoord(x int, y int) {
+func defcoord(x, y, n int) {
 	canvas.Circle(x, y, coordsize, grayfill)
-	canvas.Text(x, y-textsize, coordstring, legendstyle)
+	canvas.Text(x, y+n, coordstring, legendstyle)
 }
 
 func deflegend(x int, y int, size int, legend string) {
 	canvas.Text(x, y+size+textsize, legend, titlestyle)
 }
 
-func defcircle(id string, r int, legend string) {
+func defcircle(id string, w, h int, legend string) {
 	canvas.Gid(id)
-	defcoord(0, 0)
-	canvas.Circle(0, 0, r, objstyle)
-	canvas.Line(0, 0, r, 0, linestyle)
-	canvas.Text((r / 2), textsize, "r", legendstyle)
-	deflegend(0, 0, r, legend)
+	canvas.Translate(w, h)
+	defcoord(0, 0, -textsize)
+	canvas.Circle(0, 0, h, objstyle)
+	canvas.Line(0, 0, h, 0, linestyle)
+	canvas.Text(h/2, textsize, "r", legendstyle)
+	deflegend(0, 0, h, legend)
+	canvas.Gend()
 	canvas.Gend()
 }
 
 func defellipse(id string, w int, h int, legend string) {
 	canvas.Gid(id)
-	defcoord(0, 0)
+	canvas.Translate(w, h)
+	defcoord(0, 0, -textsize)
 	canvas.Ellipse(0, 0, w, h, objstyle)
 	canvas.Line(0, 0, w, 0, linestyle)
 	canvas.Line(0, 0, 0, h, linestyle)
-	canvas.Text((w / 2), textsize, "rx", legendstyle)
+	canvas.Text(w/2, textsize, "rx", legendstyle)
 	canvas.Text(-textsize, (h / 2), "ry", legendstyle)
 	deflegend(0, 0, h, legend)
+	canvas.Gend()
 	canvas.Gend()
 }
 
 func defrect(id string, w int, h int, legend string) {
 	canvas.Gid(id)
-	defcoord(0, 0)
-	defcoord(w/2, h/2)
+	defcoord(0, 0, -textsize)
 	canvas.Rect(0, 0, w, h, objstyle)
 	canvas.Text(-textsize, (h / 2), "h", legendstyle)
 	canvas.Text((w / 2), -textsize, "w", legendstyle)
 	deflegend((w / 2), 0, h, legend)
-	deflegend(w/2, 0, h+(textsize)+(textsize/2), "CenterRect(x, y, w, h, ...style)")
 	canvas.Gend()
 }
 
 func defcrect(id string, w int, h int, legend string) {
 	canvas.Gid(id)
-	defcoord(0, 0)
-	defcoord(w/2, h/2)
+	defcoord(w/2, h/2, -textsize)
 	canvas.Rect(0, 0, w, h, objstyle)
 	canvas.Text(-textsize, (h / 2), "h", legendstyle)
 	canvas.Text((w / 2), -textsize, "w", legendstyle)
@@ -89,7 +90,7 @@ func defcrect(id string, w int, h int, legend string) {
 
 func defsquare(id string, w int, legend string) {
 	canvas.Gid(id)
-	defcoord(0, 0)
+	defcoord(0, 0, -textsize)
 	canvas.Square(0, 0, w, objstyle)
 	canvas.Text((w / 2), -textsize, "w", legendstyle)
 	deflegend((w / 2), 0, w, legend)
@@ -98,7 +99,7 @@ func defsquare(id string, w int, legend string) {
 
 func defimage(id string, w int, h int, s string, legend string) {
 	canvas.Gid(id)
-	defcoord(0, 0)
+	defcoord(0, 0, -textsize)
 	canvas.Rect(0, 0, w, h, objstyle)
 	canvas.Text(-textsize, (h / 2), "h", legendstyle)
 	canvas.Text((w / 2), -textsize, "w", legendstyle)
@@ -121,45 +122,56 @@ func defarc(id string, w int, h int, legend string) {
 	defcoordstr(0, 0, "sx, sy")
 	defcoordstr(w*2, 0, "ex, ey")
 	canvas.Arc(0, 0, w, h, 0, false, true, w*2, 0, objstyle)
-	deflegend(w, 0, 0, legend)
+	deflegend(w, h, 0, legend)
 	canvas.Gend()
 }
 
-func defbez(id string, px int, py int, legend string) {
+func defbez(id string, x int, y int, legend string) {
+	sx, sy := 0, 0
+	cx, cy := x, -y
+	px, py := x, y
+	ex, ey := x*2, 0
 	canvas.Gid(id)
-	defcoordstr(0, 0, "sx, sy")
-	defcoordstr(px, -py, "cx, cy")
+	defcoordstr(sx, sy, "sx, sy")
+	defcoordstr(cx, cy, "cx, cy")
 	defcoordstr(px, py, "px, py")
-	defcoordstr(px*2, 0, "ex, ey")
-	canvas.Bezier(0, 0, px, -py, px, py, px*2, 0, objstyle)
-	deflegend(px, py, 10, legend)
+	defcoordstr(ex, ey, "ex, ey")
+	canvas.Bezier(sx, sy, cx, cy, px, py, ex, ey, objstyle)
+	deflegend(px, py, 0, legend)
 	canvas.Gend()
 }
 
-func defqbezier(id string, ex int, ey int, legend string) {
+func defqbezier(id string, x int, y int, legend string) {
+	sx, sy := 0, 0
+	cx, cy := x, -y
+	px, py := x, y
+	tx, ty := x*2, 0
 	canvas.Gid(id)
-	defcoordstr(0, 0, "sx, sy")
-	defcoordstr(ex, -ey, "cx, cy")
-	defcoordstr(ex, ey, "ex, ey")
-	defcoordstr(ex*2, 0, "tx, ty")
-	canvas.Qbezier(0, 0, ex, -ey, ex, ey, ex*2, 0, objstyle)
-	deflegend(ex, ey, 40, legend)
+	defcoordstr(sx, sy, "sx, sy")
+	defcoordstr(cx, cy, "cx, cy")
+	defcoordstr(px, py, "px, py")
+	defcoordstr(tx, ty, "tx, ty")
+	canvas.Qbezier(sx, sy, cx, cy, px, py, tx, ty, objstyle)
+	deflegend(x, y, 40, legend)
 	canvas.Gend()
 }
 
 func defqbez(id string, px int, py int, legend string) {
+	sx, sy := 0, 0
+	ex, ey := px*2, 0
+	cx, cy := (ex-px)/3, -py-(py/2)
 	canvas.Gid(id)
-	defcoordstr(0, 0, "sx, sy")
-	defcoordstr(px, py, "cx, cy")
-	defcoordstr(px*2, 0, "ex, ey")
-	canvas.Qbez(0, 0, px, py, px*2, 0, objstyle)
-	deflegend(px, py, 10, legend)
+	defcoordstr(sx, sy, "sx, sy")
+	defcoordstr(cx, cy, "cx, cy")
+	defcoordstr(ex, ey, "ex, ey")
+	canvas.Qbez(sx, sy, cx, cy, ex, ey, objstyle)
+	deflegend(px, py, 0, legend)
 	canvas.Gend()
 }
 
 func defroundrect(id string, w int, h int, rx int, ry int, legend string) {
 	canvas.Gid(id)
-	defcoord(0, 0)
+	defcoord(0, 0, -textsize)
 	canvas.Roundrect(0, 0, w, h, rx, ry, objstyle)
 	canvas.Text(-textsize, (h / 2), "h", legendstyle)
 	canvas.Text((w / 2), -textsize, "w", legendstyle)
@@ -173,13 +185,13 @@ func defroundrect(id string, w int, h int, rx int, ry int, legend string) {
 
 func defpolygon(id string, w int, h int, legend string) {
 	var x = []int{0, w / 2, w, w, w / 2, 0}
-	var y = []int{0, h / 5, 0, (h * 3) / 4, h, (h * 3) / 4}
+	var y = []int{0, -h / 4, 0, (h * 3) / 4, h / 2, (h * 3) / 4}
 	canvas.Gid(id)
 	for i := 0; i < len(x); i++ {
-		defcoord(x[i], y[i])
+		defcoord(x[i], y[i], -textsize)
 	}
 	canvas.Polygon(x, y, objstyle)
-	deflegend(x[4], y[4], 10, legend)
+	deflegend(w/2, h, 0, legend)
 	canvas.Gend()
 }
 
@@ -188,7 +200,7 @@ func defpolyline(id string, w int, h int, legend string) {
 	var y = []int{0, -(h / 2), -(h / 3), -h}
 	canvas.Gid(id)
 	for i := 0; i < len(x); i++ {
-		defcoord(x[i], y[i])
+		defcoord(x[i], y[i], -textsize)
 	}
 	canvas.Polyline(x, y, objstyle)
 	deflegend(x[1], y[1], 30, legend)
@@ -201,7 +213,7 @@ func defpath(id string, x, y int, legend string) {
 	canvas.Gid(id)
 	canvas.Path(w3path, `fill="#AA0000"`)
 	canvas.Path(cpath)
-	defcoord(0, 0)
+	defcoord(0, 0, -textsize)
 	deflegend(x/2, y, 10, legend)
 	canvas.Gend()
 }
@@ -230,29 +242,36 @@ func deftrans(id string, w, h int, legend string) {
 	defcoordstr(0, 0, "0, 0")
 	defcoordstr(w-tx, 0, "x, y")
 	deflegend(w/2, 0, h, legend)
-	canvas.Rect(0, 0, tx, h, objstyle)
-	canvas.Translate(w-tx, 0)
 	canvas.Rect(0, 0, tx, h, fobjstyle)
+	canvas.Translate(w-tx, 0)
+	canvas.Rect(0, 0, tx, h, objstyle)
 	canvas.Gend()
 	canvas.Gend()
 }
 
 func defgrid(id string, w, h int, legend string) {
+	n := 50
 	canvas.Gid(id)
-	canvas.Grid(0, 0, w, h, 25, "stroke:rgb(127,0,0)")
+	defcoord(0, 0, -textsize)
+	canvas.Text(-textsize, (h / 2), "h", legendstyle)
+	canvas.Text((w / 2), -textsize, "w", legendstyle)
+	canvas.Text(n+textsize, n/2, "n", legendstyle)
+	canvas.Grid(0, 0, w, h, n, "stroke:rgb(127,0,0)")
 	deflegend((w / 2), 0, h, legend)
 	canvas.Gend()
 }
 
 func deftext(id string, w, h int, text string, legend string) {
 	canvas.Gid(id)
-	canvas.Text(w/2, h, "hello", "font-size:48pt")
+	defcoord(0, h/2, textsize)
+	canvas.Text(0, h/2, text, "text-anchor:start;font-size:32pt")
 	deflegend(w/2, 0, h, legend)
 	canvas.Gend()
 }
 
 func defscale(id string, w, h int, n float64, legend string) {
 	canvas.Gid(id)
+	defcoordstr(0, 0, "0, 0")
 	canvas.Rect(0, 0, w, h, objstyle)
 	canvas.Scale(n)
 	canvas.Rect(0, 0, w, h, fobjstyle)
@@ -263,6 +282,7 @@ func defscale(id string, w, h int, n float64, legend string) {
 
 func defrotate(id string, w, h int, n float64, legend string) {
 	canvas.Gid(id)
+	defcoordstr(0, 0, "0, 0")
 	deflegend(w/2, 0, h, legend)
 	canvas.Rect(0, 0, w, h, fobjstyle)
 	canvas.Rotate(n)
@@ -271,109 +291,70 @@ func defrotate(id string, w, h int, n float64, legend string) {
 	canvas.Gend()
 }
 
-func deftr(id string, tx, ty int, r float64, legend string) {
-	canvas.Gid(id)
-	canvas.Gend()
-}
-
-func defrt(id string, tx, ty int, r float64, legend string) {
-	canvas.Gid(id)
-	canvas.Gend()
-}
-
-func main() {
-	width := 1400
-	height := 1400
-	canvas.Start(width, height)
-	canvas.Title("SVG Go Library Description")
+func defobjects() {
+	canvas.Desc("Object Definitions")
 	canvas.Def()
 	canvas.LinearGradient("linear", 0, 0, 100, 0, ga)
 	canvas.RadialGradient("radial", 0, 0, 100, 50, 50, ga)
-	canvas.DefEnd()
-	canvas.Rect(0, 0, width, height, "fill:white;stroke:black;stroke-width:2")
-	canvas.Gstyle(gtextstyle)
-	canvas.Text(width/2, 100, "SVG Go Library", "font-size:50px")
-
-	canvas.Desc("Object Definitions")
-	canvas.Def()
 	defsquare("square", 100, "Square(x, y, w,...style)")
-	defrect("rectangle", 150, 100, "Rect(x, y, w, h,...style)")
-	defcrect("crect", 150, 100, "CenterRect(x, y, w, h,...style)")
-	defroundrect("roundrect", 200, 100, 25, 25, "Roundrect(x,y,w,h,rx,ry, ...style)")
-	defpolygon("polygon", 200, 120, "Polygon(x, y, ...style)")
-
-	defcircle("circle", 50, "Circle(x, y, r,...style)")
-	defellipse("ellipse", 75, 50, "Ellipse(x, y, rx ,ry,...style)")
+	defrect("rect", 200, 100, "Rect(x, y, w, h,...style)")
+	defcrect("crect", 200, 100, "CenterRect(x, y, w, h,...style)")
+	defroundrect("roundrect", 200, 100, 25, 25, "Roundrect(x, y, w, h, rx, ry, ...style)")
+	defpolygon("polygon", 200, 100, "Polygon(x, y, ...style)")
+	defcircle("circle", 100, 50, "Circle(x, y, r, ...style)")
+	defellipse("ellipse", 100, 50, "Ellipse(x, y, rx, ry, ...style)")
 	defline("line", 200, "Line(x1, y1, x2, y2, ...style)")
 	defpolyline("polyline", 200, 50, "Polyline(x, y, ...style)")
-
-	defarc("arc", 50, 40, "Arc(sx, sy, ax, ay, r, lflag, sflag, ex, ey, ...style)")
+	defarc("arc", 100, 50, "Arc(sx, sy, ax, ay, r, lflag, sflag, ex, ey, ...style)")
 	defpath("path", 100, 80, "Path(s, ...style)")
-	defqbez("qbez", 100, 80, "Qbez(sx, sy, cx, cy, ex, ey, ...style)")
+	defqbez("qbez", 100, 50, "Qbez(sx, sy, cx, cy, ex, ey, ...style)")
 	defqbezier("qbezier", 100, 50, "Qbezier(sx, sy, cx, cy, ex, ey, tx, ty, ...style)")
-
 	defbez("bezier", 100, 50, "Bezier(sx, sy, cx, cy, px, py, ex, ey, ...style)")
 	defimage("image", 128, 128, "images/gophercolor128x128.png", "Image(x, y, w, h, path, ...style)")
-	deflg("lgrad", 200, 100, "LinearGradient(id, x1, y1, x2, y2, Offcolor)")
-	defrg("rgrad", 200, 100, "RadialGradient(id, cx, cy, r, fx, fy, Offcolor)")
-
+	deflg("lgrad", 200, 100, "LinearGradient(id, x1, y1, x2, y2, oc)")
+	defrg("rgrad", 200, 100, "RadialGradient(id, cx, cy, r, fx, fy, oc)")
 	deftrans("trans", 200, 100, "Translate(x, y)")
 	defgrid("grid", 200, 100, "Grid(x, y, w, h, n, ...style)")
-	deftext("text", 200, 100, "hello", "Text(x, y, s, ...style)")
+	deftext("text", 200, 100, "hello, SVG", "Text(x, y, s, ...style)")
 	defscale("scale", 200, 100, 0.5, "Scale(n)")
 	defrotate("rotate", 200, 100, 30, "Rotate(n)")
 	canvas.DefEnd()
+}
 
+func placerow(w, h int, s []string) {
+	for x, name := range s {
+		canvas.Use(x*w, 0, "#"+name)
+	}
+}
+
+func placeobjects(x, y, w, h int, data [][]string) {
 	canvas.Desc("Object Usage")
+	for _, object := range data {
+		canvas.Translate(x, y)
+		placerow(w, h, object)
+		canvas.Gend()
+		y += h
+	}
+}
 
-	left := 140
-	y := 200
-	rowsize := y + 50
-	
-	// row 0
-	canvas.Translate(left, y)
-	canvas.Use(0, 0,    "#square")
-	canvas.Use(300, 0,  "#rectangle")
-	canvas.Use(600, 0,  "#roundrect")
-	canvas.Use(950, 0,  "#polygon")
-	canvas.Gend()
-	y += rowsize
 
-	// row 1
-	canvas.Translate(left, y)
-	canvas.Use(50, 0,   "#circle")
-	canvas.Use(375, 0,  "#ellipse")
-	canvas.Use(600, 0,  "#line")
-	canvas.Use(950, 0,  "#polyline")
-	canvas.Gend()
-	y += rowsize
+var roworder = [][]string{
+	{"rect", "crect", "roundrect", "square", "line", "polyline"},
+	{"polygon", "circle", "ellipse", "arc", "qbez", "bezier"},
+	{"lgrad", "rgrad", "trans", "scale", "rotate", "grid"},
+	{"text", "image", "path"},
+}
 
-	// row 2
-	canvas.Translate(left, y)
-	canvas.Use(0, 0,    "#arc")
-	canvas.Use(300, 0,  "#path")
-	canvas.Use(600, 0,  "#qbez")
-	canvas.Use(950, 0,  "#qbezier")
-	canvas.Gend()
-	y += rowsize
-
-	// row 3
-	canvas.Translate(left, y)
-	canvas.Use(0, 0,    "#bezier")
-	canvas.Use(300, 0,  "#image")
-	canvas.Use(600, 0,  "#lgrad")
-	canvas.Use(950, 0, "#rgrad")
-	canvas.Gend()
-	y += rowsize
-
-	// row 4 
-	canvas.Translate(left, y)
-	canvas.Use(0, 0,    "#trans")
-	canvas.Use(300, 0,  "#scale")
-	canvas.Use(600, 0,  "#rotate")
-	canvas.Use(950, 0,  "#grid")
-	canvas.Gend()
-
+func main() {
+	width := 2200
+	height := (width * 3) / 4
+	canvas.Start(width, height)
+	defobjects()
+	canvas.Title("SVG Go Library Description")
+	canvas.Rect(0, 0, width, height, "fill:white;stroke:black;stroke-width:2")
+	canvas.Gstyle(gtextstyle)
+	canvas.Text(width/2, 75, "SVG Go Library", "font-size:50px")
+	placeobjects(120, 200, 350, 400, roworder)
 	canvas.Gend()
 	canvas.End()
 
