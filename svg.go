c72a19726d92c090ccb3e7e01ee50cd1de28fe80
Anthony Starks <ajstarks@gmail.com>
2011-09-08 06:37:50 -0400

    example comment fix

--- svg.go
+++ svg.go
@@ -17,7 +17,8 @@ package svg
 // 	func main() {
 // 		canvas.Start(width, height)
 // 		canvas.Circle(width/2, height/2, 100)
-// 		canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor;font-size:30px;fill:white")
+// 		canvas.Text(width/2, height/2, "Hello, SVG", 
+//                   "text-anchor:middle;font-size:30px;fill:white")
 // 		canvas.End()
 // 	}
 //
