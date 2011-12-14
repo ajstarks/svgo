9585619f1c5c47039ad1d2b50e23dc121c501406
Anthony Starks <ajstarks@gmail.com>
2011-12-14 18:29:56 -0500

    colortab cleanup

--- colortab/colortab.go
+++ colortab/colortab.go
@@ -1,45 +1,43 @@
+// colortab -- make a color/code placemat
+
 package main
 
 import (
 	"fmt"
 	"os"
-	"github.com/ajstarks/svgo"
 	"strings"
 	"bufio"
 	"flag"
-)
-
-var (
-	canvas   = svg.New(os.Stdout)
-	filename = flag.String("f", "svgcolors.txt", "input file")
-	fontname = flag.String("font", "Calibri,sans-serif", "fontname")
-	outline  = flag.Bool("o", false, "outline")
-	neg      = flag.Bool("n", false, "negative")
-	showrgb  = flag.Bool("rgb", true, "show RGB")
-	circsw   = flag.Bool("circle", true, "circle swatch")
-	fontsize = flag.Int("fs", 12, "fontsize")
-	width    = flag.Int("w", 1600, "width")
-	height   = flag.Int("h", 900, "height")
-	rowsize  = flag.Int("r", 32, "rowsize")
-	colw     = flag.Int("c", 320, "column size")
-	swatch   = flag.Int("s", 16, "swatch size")
-	gutter   = flag.Int("g", 11, "gutter")
+	"github.com/ajstarks/svgo"
 )
 
 func main() {
-	top := 32
-	left := 32
 	var (
-		colorfmt, tcolor, line string
+		canvas                          = svg.New(os.Stdout)
+		filename                        = flag.String("f", "svgcolors.txt", "input file")
+		fontname                        = flag.String("font", "Calibri,sans-serif", "fontname")
+		outline                         = flag.Bool("o", false, "outline")
+		neg                             = flag.Bool("n", false, "negative")
+		showrgb                         = flag.Bool("rgb", true, "show RGB")
+		showcode                        = flag.Bool("showcode", true, "only show colors")
+		circsw                          = flag.Bool("circle", true, "circle swatch")
+		fontsize                        = flag.Int("fs", 12, "fontsize")
+		width                           = flag.Int("w", 1600, "width")
+		height                          = flag.Int("h", 900, "height")
+		rowsize                         = flag.Int("r", 32, "rowsize")
+		colw                            = flag.Int("c", 320, "column size")
+		swatch                          = flag.Int("s", 16, "swatch size")
+		gutter                          = flag.Int("g", 11, "gutter")
 		err                    os.Error = nil
+		colorfmt, tcolor, line string
 	)
+
 	flag.Parse()
 	f, oerr := os.Open(*filename)
 	if oerr != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", oerr)
 		return
 	}
-	in := bufio.NewReader(f)
 	canvas.Start(*width, *height)
 	if *neg {
 		canvas.Rect(0, 0, *width, *height, "fill:black")
@@ -48,9 +46,11 @@ func main() {
 		canvas.Rect(0, 0, *width, *height, "fill:white")
 		tcolor = "black"
 	}
-	canvas.Gstyle(fmt.Sprintf("font-family:%s;font-size:%dpt;fill:%s;stroke-width:2",
+	top := 32
+	left := 32
+	in := bufio.NewReader(f)
+	canvas.Gstyle(fmt.Sprintf("font-family:%s;font-size:%dpt;fill:%s",
 		*fontname, *fontsize, tcolor))
-
 	for x, y, nr := left, top, 0; err == nil; nr++ {
 		line, err = in.ReadString('\n')
 		fields := strings.Split(strings.TrimSpace(line), "\t")
@@ -61,16 +61,20 @@ func main() {
 		if len(fields) == 3 {
 			colorfmt = "fill:" + fields[1]
 			if *outline {
-				colorfmt = colorfmt + ";stroke:" + tcolor
+				colorfmt = colorfmt + ";stroke-width:1;stroke:" + tcolor
 			}
 			if *circsw {
 				canvas.Circle(x, y, *swatch/2, colorfmt)
 			} else {
-				canvas.Square(x, y-*swatch/2, *swatch, colorfmt)
+				canvas.CenterRect(x, y, *swatch, *swatch, colorfmt)
 			}
 			canvas.Text(x+*swatch+*fontsize/2, y+(*swatch/4), fields[0], "stroke:none")
-			if *showrgb {
-				canvas.Text(x+((*colw*4)/5), y+(*swatch/4), fields[2], "text-anchor:end;fill:gray")
+			if *showcode {
+				if *showrgb {
+					canvas.Text(x+((*colw*4)/5), y+(*swatch/4), fields[2], "text-anchor:end;fill:gray")
+				} else {
+					canvas.Text(x+((*colw*4)/5), y+(*swatch/4), fields[1], "text-anchor:end;fill:gray")
+				}
 			}
 		}
 		y += (*swatch + *gutter)
