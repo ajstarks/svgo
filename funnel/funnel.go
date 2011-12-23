454f74290ca2d9a581ea66f1e3731415dbc1efc5
Anthony Starks <ajstarks@gmail.com>
2011-12-23 05:56:39 -0500

    make funnel use import path

--- funnel/funnel.go
+++ funnel/funnel.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	svglib "./svg"
+	"github.com/ajstarks/svgo"
 	"os"
 )
 
-var svg = svglib.New(os.Stdout)
-
+var canvas = svg.New(os.Stdout)
 var width = 320
 var height = 480
 
 func funnel(bg int, fg int, grid int, dim int) {
 	h := dim / 2
-	svg.Rect(0, 0, width, height, svg.RGB(bg, bg, bg))
+	canvas.Rect(0, 0, width, height, canvas.RGB(bg, bg, bg))
 	for size := grid; size < width; size += grid {
-		svg.Ellipse(h, size, size/2, size/2, svg.RGBA(fg, fg, fg, 0.2))
+		canvas.Ellipse(h, size, size/2, size/2, canvas.RGBA(fg, fg, fg, 0.2))
 	}
 }
 
 func main() {
-	svg.Start(width, height)
-	svg.Title("Funnel")
+	canvas.Start(width, height)
+	canvas.Title("Funnel")
 	funnel(0, 255, 25, width)
-	svg.End()
+	canvas.End()
 }
