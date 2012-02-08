fe097f34572a232868ff9c31a9ef5f74bf8e94d9
Anthony Starks <ajstarks@gmail.com>
2012-02-07 22:14:40 -0500

    codepic does syntax higlighting

--- codepic/codepic.go
+++ codepic/codepic.go
@@ -19,9 +19,18 @@ var (
 	font                                                      string
 	codeframe, picframe                                       bool
 	linespacing, fontsize, top, left, boxwidth, width, height int
-	framestyle                                                = "stroke:gray;stroke-dasharray:1,1;fill:none"
-	labelstyle                                                = "text-anchor:middle"
-	codefmt                                                   = "font-family:%s;font-size:%dpx"
+)
+
+const (
+	framestyle = "stroke:gray;stroke-dasharray:1,1;fill:none"
+	labelstyle = "text-anchor:middle"
+	codefmt    = "font-family:%s;font-size:%dpx"
+	labelfmt   = "text-anchor:middle;" + codefmt
+	kwfmt      = `<tspan %s>%s</tspan>`
+	commentfmt = `<tspan font-style="italic" fill="rgb(127,127,127)">%s</tspan>`
+	textfmt    = "<text x=\"%d\" y=\"%d\" xml:space=\"preserve\">%s</text>\n"
+	svgofmt    = `font-weight="bold"  fill="rgb(0,0,127)"`
+	gokwfmt    = `font-style="italic" fill="rgb(127,0,0)"`
 )
 
 // incoming SVG file, capture everything into between <svg..> and </svg> 
@@ -30,6 +39,22 @@ type SVG struct {
 	Width  int    `xml:"width,attr"`
 	Height int    `xml:"height,attr"`
 	Doc    string `xml:",innerxml"`
+}
+
+var gokw = []string{
+	"defer ", "go ", "range ", "chan ", " continue", "if ", "for ", "func ",
+	"uint8", "uint", "uint16", "uint32", "complex64", "complex128", " byte", "int8", "int16", "int32",
+	"int64", " int", "float64", "float32", " string", "import ", "const ",
+	"package ", "return", "var ", "type ", "switch ", "case ", "default:",
+}
+
+var svgokw = []string{
+	".Start", ".Startview,", ".End", ".Script", ".Gstyle", ".Gtransform", ".Scale", ".Offcolor",
+	".ScaleXY", ".SkewX", ".SkewY", ".SkewXY,", ".Rotate", ".TranslateRotate", ".RotateTranslate", ".Translate",
+	".Group", ".Gid", ".Gend", ".ClipPath", ".ClipEnd", ".DefEnd", ".Def", ".Desc", ".Title", ".Linkf",
+	".LinkEnd", ".Use", ".Mask", ".MaskEnd", ".Circle", ".Ellipse", ".Polygon", ".Rect", ".CenterRect",
+	".Roundrect", ".Square", ".Path", ".Arc", ".Bezier", ".Qbez", ".Qbezier", ".Line", ".Polyline", ".Image",
+	".Textpath", ".Textlines,", ".Text", ".RGBA", ".RGB", ".LinearGradient", ".RadialGradient", ".Grid",
 }
 
 // codepic makes a code+picture SVG file, given a go source file
@@ -58,6 +83,7 @@ func codepic(filename string) {
 func placecode(x, y int, filename string) {
 	var rerr error
 	var line string
+	var ic bool
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
@@ -65,15 +91,44 @@ func placecode(x, y int, filename string) {
 	}
 	defer f.Close()
 	in := bufio.NewReader(f)
+
 	for xp := left + fontsize; rerr == nil; y += linespacing {
 		line, rerr = in.ReadString('\n')
 		if len(line) > 0 {
-			canvas.Text(xp, y, line[0:len(line)-1], `xml:space="preserve"`)
+			line, ic = svgtext(xp, y, line[0:len(line)-1])
+			if !ic {
+				line = keyword(line, gokwfmt, gokw)
+				line = keyword(line, svgofmt, svgokw)
+			}
+			io.WriteString(canvas.Writer, line)
 		}
 	}
 	if codeframe {
 		canvas.Rect(top, left, left+boxwidth, y, framestyle)
 	}
+}
+
+// keyword styles keywords in a line of code 
+func keyword(line string, style string, kw []string) string {
+	for _, k := range kw {
+		line = strings.Replace(line, k, fmt.Sprintf(kwfmt, style, k), 1)
+	}
+	return line
+}
+
+// svgtext 
+func svgtext(x, y int, s string) (string, bool) {
+	var iscomment = false
+	s = strings.Replace(s, "&", "&amp;", -1)
+	s = strings.Replace(s, "<", "&lt;", -1)
+	s = strings.Replace(s, ">", "&gt;", -1)
+
+	i := strings.Index(s, "// ")
+	if i >= 0 {
+		iscomment = true
+		s = strings.Replace(s, s[i:], fmt.Sprintf(commentfmt, s[i:]), 1)
+	}
+	return fmt.Sprintf(textfmt, x, y, s), iscomment
 }
 
 // placepic places the picture on the right
@@ -89,7 +144,7 @@ func placepic(x, y int, basename string) {
 		fmt.Fprintf(os.Stderr, "Unable to parse (%v)\n", err)
 		return
 	}
-	canvas.Text(x+s.Width/2, height-10, basename+".go", fmt.Sprintf(codefmt, font, fontsize*2))
+	canvas.Text(x, height-10, basename+".go", fmt.Sprintf(labelfmt, font, fontsize*2))
 	canvas.Group(`clip-path="url(#pic)"`, fmt.Sprintf(`transform="translate(%d,%d)"`, x, y))
 	canvas.ClipPath(`id="pic"`)
 	canvas.Rect(0, 0, s.Width, s.Height)
@@ -103,8 +158,8 @@ func placepic(x, y int, basename string) {
 
 // init initializes flags
 func init() {
-	flag.BoolVar(&codeframe, "codeframe", true, "frame the code")
-	flag.BoolVar(&picframe, "picframe", true, "frame the picture")
+	flag.BoolVar(&codeframe, "codeframe", false, "frame the code")
+	flag.BoolVar(&picframe, "picframe", false, "frame the picture")
 	flag.IntVar(&width, "w", 1024, "width")
 	flag.IntVar(&height, "h", 768, "height")
 	flag.IntVar(&linespacing, "ls", 16, "linespacing")
