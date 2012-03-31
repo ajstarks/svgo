f7367c166fa486e97215923355f7af873fa1626c
Anthony Starks <ajstarks@gmail.com>
2012-03-31 15:13:45 -0400

    update package comment

--- bulletgraph/bulletgraph.go
+++ bulletgraph/bulletgraph.go
@@ -1,4 +1,4 @@
-// bulletgraph - bullet graphs (Bullet Graph Design Specification http://www.perceptualedge.com/articles/misc/Bullet_Graph_Design_Spec.pdf)
+// bulletgraph - bullet graphs (Design Specification http://www.perceptualedge.com/articles/misc/Bullet_Graph_Design_Spec.pdf)
 package main
 
 import (
