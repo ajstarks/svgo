98e80ae76ab1927a2823c612b6039c25fde5dd3b
Anthony Starks <ajstarks@gmail.com>
2012-04-13 22:50:42 -0400

    svgplot: added horizon plot, use polyline and polygon, label updates

--- svgplot/svgplot.go
+++ svgplot/svgplot.go
@@ -39,7 +39,8 @@ var (
 
 const (
 	globalfmt = "font-family:%s;font-size:%dpt;stroke-width:%dpx"
-	linefmt   = "stroke:%s"
+	linestyle = "fill:none;stroke:"
+	linefmt   = "fill:none;stroke:%s"
 	barfmt    = linefmt + ";stroke-width:%dpx"
 	ticfmt    = "stroke:rgb(200,200,200);stroke-width:1px"
 	labelfmt  = ticfmt + ";text-anchor:end;fill:black"
@@ -48,23 +49,27 @@ const (
 
 // init initializes command flags and sets default options
 func init() {
+
 	// boolean options 
 	showx := flag.Bool("showx", true, "show the xaxis")
 	showy := flag.Bool("showy", true, "show the yaxis")
 	showbar := flag.Bool("showbar", false, "show data bars")
+	horizon := flag.Bool("horizon", false, "horizon chart")
 	connect := flag.Bool("connect", true, "connect data points")
 	showdot := flag.Bool("showdot", false, "show dots")
 	showbg := flag.Bool("showbg", true, "show the background color")
 	showfile := flag.Bool("showfile", false, "show the filename")
-	
+
 	// attributes
 	bgcolor := flag.String("bgcolor", "rgb(240,240,240)", "plot background color")
 	barcolor := flag.String("barcolor", "gray", "bar color")
 	dotcolor := flag.String("dotcolor", "black", "dot color")
 	linecolor := flag.String("linecolor", "gray", "line color")
+	hcolor := flag.String("hcolor", "gray", "horizon color")
 	font := flag.String("font", "Calibri,sans", "font")
+	labelcolor := flag.String("labelcolor", "black", "label color")
 	plotlabel := flag.String("label", "", "plot label")
-	
+
 	// sizes
 	dotsize := flag.Int("dotsize", 2, "dot size")
 	linesize := flag.Int("linesize", 2, "line size")
@@ -89,6 +94,7 @@ func init() {
 	plotopt["showx"] = *showx
 	plotopt["showy"] = *showy
 	plotopt["showbar"] = *showbar
+	plotopt["horizon"] = *horizon
 	plotopt["connect"] = *connect
 	plotopt["showdot"] = *showdot
 	plotopt["showbg"] = *showbg
@@ -98,8 +104,10 @@ func init() {
 	plotattr["barcolor"] = *barcolor
 	plotattr["linecolor"] = *linecolor
 	plotattr["dotcolor"] = *dotcolor
+	plotattr["hcolor"] = *hcolor
 	plotattr["font"] = *font
 	plotattr["label"] = *plotlabel
+	plotattr["labelcolor"] = *labelcolor
 
 	plotnum["dotsize"] = *dotsize
 	plotnum["linesize"] = *linesize
@@ -131,22 +139,26 @@ func doplot(x, y int, location string) {
 		return
 	}
 	nd, data := readxy(f)
+	f.Close()
+
 	if nd >= 2 {
 		plot(x, y, plotw, ploth, ps, data)
 	}
-	f.Close()
 }
 
 // plot places a plot at the specified location with the specified dimemsions
 // usinng the specified settings, using the specified data
 func plot(x, y, w, h int, settings plotset, d []rawdata) {
-	if len(d) < 2 {
+	nd := len(d)
+	if nd < 2 {
 		fmt.Fprintf(os.Stderr, "%d is not enough points to plot\n", len(d))
 		return
 	}
+	// Compute the minima and maxima
 	maxx, minx := d[0].x, d[0].x
 	maxy, miny := d[0].y, d[0].y
 	for _, v := range d {
+
 		if v.x > maxx {
 			maxx = v.x
 		}
@@ -160,19 +172,36 @@ func plot(x, y, w, h int, settings plotset, d []rawdata) {
 			miny = v.y
 		}
 	}
+	// Prepare for a horizon or line chart by allocating
+	// polygon coordinates; for the hrizon plot, you need two extra coordinates
+	// for the extrema.
+	needpoly := settings.opt["horizon"] || settings.opt["connect"]
+	var xpoly, ypoly []int
+	if needpoly {
+		xpoly = make([]int, nd+2)
+		ypoly = make([]int, nd+2)
+		// preload the extrema of the polygon, 
+		// the bottom left and bottom right of the plot's rectangle
+		xpoly[0] = x
+		ypoly[0] = y + h
+		xpoly[nd+1] = x + w
+		ypoly[nd+1] = y + h
+	}
+	// Draw the plot's bounding rectangle
 	if settings.opt["showbg"] {
 		canvas.Rect(x, y, w, h, "fill:"+settings.attr["bgcolor"])
 	}
+	// Loop through the data, drawing items as specified
 	spacer := 10
-	canvas.Gstyle(fmt.Sprintf(globalfmt,
-		settings.attr["font"], settings.size["fontsize"], settings.size["linesize"]))
-	if len(settings.attr["label"]) > 0 {
-		canvas.Text(x, y-spacer, settings.attr["label"], textfmt+";font-size:120%")
-	}
-	px, py := 0, 0
+	canvas.Gstyle(fmt.Sprintf(globalfmt, settings.attr["font"], settings.size["fontsize"], settings.size["linesize"]))
 	for i, v := range d {
 		xp := int(fmap(v.x, minx, maxx, float64(x), float64(x+w)))
 		yp := int(fmap(v.y, miny, maxy, float64(y), float64(y-h)))
+
+		if needpoly {
+			xpoly[i+1] = xp
+			ypoly[i+1] = yp + h
+		}
 		if settings.opt["showbar"] {
 			canvas.Line(xp, yp+h, xp, y+h,
 				fmt.Sprintf(barfmt, settings.attr["barcolor"], settings.size["barsize"]))
@@ -180,18 +209,22 @@ func plot(x, y, w, h int, settings plotset, d []rawdata) {
 		if settings.opt["showdot"] {
 			canvas.Circle(xp, yp+h, settings.size["dotsize"], "fill:"+settings.attr["dotcolor"])
 		}
-		if settings.opt["connect"] && i > 0 && i < len(d) {
-			canvas.Line(xp, yp+h, px, py+h, fmt.Sprintf(linefmt, settings.attr["linecolor"]))
-		}
 		if settings.opt["showx"] {
 			if i%settings.size["xinterval"] == 0 {
 				canvas.Text(xp, (y+h)+(spacer*2), fmt.Sprintf("%d", int(v.x)), "text-anchor:middle")
 				canvas.Line(xp, (y + h), xp, (y+h)+spacer, ticfmt)
 			}
 		}
-		px = xp
-		py = yp
 	}
+	// Done constructing the points for the horizon or line plots, display them in one shot
+	if settings.opt["horizon"] {
+		canvas.Polygon(xpoly, ypoly, "fill:"+settings.attr["hcolor"])
+	}
+	
+	if settings.opt["connect"] {
+		canvas.Polyline(xpoly[1:nd-1], ypoly[1:nd-1], linestyle+settings.attr["linecolor"])
+	}
+	// Put on the y axis labels, if specified
 	if settings.opt["showy"] {
 		bot := math.Floor(miny)
 		top := math.Ceil(maxy)
@@ -205,6 +238,11 @@ func plot(x, y, w, h int, settings plotset, d []rawdata) {
 		}
 		canvas.Gend()
 	}
+	// Finally, tack on the label, if specified
+	if len(settings.attr["label"]) > 0 {
+		canvas.Text(x, y+spacer, settings.attr["label"], "font-size:120%;fill:"+settings.attr["labelcolor"])
+	}
+
 	canvas.Gend()
 }
 
