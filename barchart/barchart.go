b6d1ac67730836cd36bb32b76e57fb7436c925b1
Anthony Starks <ajstarks@gmail.com>
2013-04-03 23:55:33 -0400

    use absolute values in data, but indicate negative values

--- barchart/barchart.go
+++ barchart/barchart.go
@@ -173,7 +173,14 @@ func drawbc(bg Barchart, canvas *svg.SVG) {
 
 		// draw the data items
 		canvas.Gstyle(datastyle + color)
+		var sign = ""
 		for _, d := range b.Bitem {
+			if d.Value < 0 {
+				d.Value = -d.Value
+				sign = "-"
+			} else {
+				sign = ""
+			}
 			canvas.Text(x-sep, y+barheight/2, d.Name, labelstyle)
 			dw := vmap(d.Value, scalemin, scalemax, 0, float64(maxwidth))
 			if len(d.Color) > 0 {
@@ -186,9 +193,9 @@ func drawbc(bg Barchart, canvas *svg.SVG) {
 				var ditem string
 				var datax int
 				if len(b.Unit) > 0 {
-					ditem = fmt.Sprintf("%v%s", d.Value, b.Unit)
+					ditem = fmt.Sprintf("%s%v%s", sign, d.Value, b.Unit)
 				} else {
-					ditem = fmt.Sprintf("%v", d.Value)
+					ditem = fmt.Sprintf("%s%s%v", sign, d.Value)
 				}
 				if len(inbar) > 0 {
 					valuestyle += inbar
