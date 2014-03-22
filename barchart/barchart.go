c752b86e129d8dc1a1cb88d2f73356d3cc8f3826
Anthony Starks <ajstarks@gmail.com>
2014-03-22 08:51:56 -0400

    add stacked bars

--- barchart/barchart.go
+++ barchart/barchart.go
@@ -15,7 +15,7 @@ import (
 
 var (
 	width, height, iscale, fontsize, barheight, gutter, cornerRadius int
-	bgcolor, barcolor, title, inbar                                  string
+	bgcolor, barcolor, title, inbar, valformat                       string
 	showtitle, showdata, showgrid, showscale, endtitle               bool
 )
 
@@ -34,14 +34,13 @@ const (
 // <barchart title="Bullet Graph" top="50" left="250" right="50">
 //    <note>This is a note</note>
 //    <note>More expository text</note>
-//    <bdata title="Browser Market Share" scale="0,100,20" showdata="on" color="red" unit="%"/>
+//    <bdata title="Browser Market Share" scale="0,100,20" showdata="true" color="red" unit="%"/>
 //    	<bitem name="Firefox"  value="22.5" color="green"/>
 //    	<bitem name="Chrome" value="12.3"/>
 //		<bitem name="IE8" value="63.5"/>
 //	  <bdata>
 // </barchart>
 
-// Barchart defines the barchart structure
 type Barchart struct {
 	Top   int     `xml:"top,attr"`
 	Left  int     `xml:"left,attr"`
@@ -51,20 +50,29 @@ type Barchart struct {
 }
 
 type bdata struct {
-	Title    string  `xml:"title,attr"`
-	Scale    string  `xml:"scale,attr"`
-	Color    string  `xml:"color,attr"`
-	Unit     string  `xml:"unit,attr"`
-	Showdata string  `xml:"showdata,attr"`
-	Showgrid string  `xml:"showgrid,attr"`
-	Bitem    []bitem `xml:"bitem"`
-	Note     []note  `xml:"note"`
+	Title    string   `xml:"title,attr"`
+	Scale    string   `xml:"scale,attr"`
+	Color    string   `xml:"color,attr"`
+	Unit     string   `xml:"unit,attr"`
+	Showdata bool     `xml:"showdata,attr"`
+	Showgrid bool     `xml:"showgrid,attr"`
+	Samebar  bool     `xml:"samebar,attr"`
+	Bitem    []bitem  `xml:"bitem"`
+	Bstack   []bstack `xml:"bstack"`
+	Note     []note   `xml:"note"`
 }
 
 type bitem struct {
-	Name  string  `xml:"name,attr"`
-	Value float64 `xml:"value,attr"`
-	Color string  `xml:"color,attr"`
+	Name    string  `xml:"name,attr"`
+	Value   float64 `xml:"value,attr"`
+	Color   string  `xml:"color,attr"`
+	Samebar bool    `xml:"samebar,attr"`
+}
+
+type bstack struct {
+	Name  string `xml:"name,attr"`
+	Value string `xml:"value,attr"`
+	Color string `xml:"color,attr"`
 }
 
 type note struct {
@@ -136,8 +144,15 @@ func drawbc(bg Barchart, canvas *svg.SVG) {
 		// if not specified, compute the scale factors
 		sc := strings.Split(b.Scale, ",")
 		var scalemin, scalemax, scaleincr float64
+
 		if len(sc) != 3 {
-			scalemin, scalemax, scaleincr = scalevalues(b.Bitem)
+			if len(b.Bitem) > 0 {
+				scalemin, scalemax, scaleincr = scalevalues(b.Bitem)
+			}
+
+			if len(b.Bstack) > 0 {
+				scalemin, scalemax, scaleincr = scalestack(b.Bstack)
+			}
 		} else {
 			scalemin, _ = strconv.ParseFloat(sc[0], 64)
 			scalemax, _ = strconv.ParseFloat(sc[1], 64)
@@ -148,11 +163,93 @@ func drawbc(bg Barchart, canvas *svg.SVG) {
 
 		y += sep * 2
 		chartop := y
-		chartbot := chartop + ((len(b.Bitem)) * (barheight + gutter))
+
+		// draw the data items
+		canvas.Gstyle(datastyle + color)
+
+		// stacked bars
+		for _, stack := range b.Bstack {
+			stackdata := stackvalues(stack.Value)
+			if len(stackdata) < 1 {
+				continue
+			}
+			sx := x
+			canvas.Text(x-sep, y+barheight/2, stack.Name, labelstyle)
+			barop := colorange(1.0, 0.3, len(stackdata))
+			for ns, sd := range stackdata {
+				dw := vmap(sd, scalemin, scalemax, 0, float64(maxwidth))
+				if len(stack.Color) > 0 {
+					canvas.Roundrect(sx, y, int(dw), barheight, cr, cr, fmt.Sprintf("fill:%s;fill-opacity:%.2f", stack.Color, barop[ns]))
+				} else {
+					canvas.Roundrect(sx, y, int(dw), barheight, cr, cr, fmt.Sprintf("fill-opacity:%.2f", barop[ns]))
+				}
+				
+				
+				if (showdata || b.Showdata) && sd > 0 {
+					var valuestyle = "fill-opacity:1;font-style:italic;font-size:75%;text-anchor:middle;baseline-shift:-25%;"
+					var ditem string
+					var datax int
+					if len(b.Unit) > 0 {
+						ditem = fmt.Sprintf(valformat+"%s", sd, b.Unit)
+					} else {
+						ditem = fmt.Sprintf(valformat, sd)
+					}
+					if len(inbar) > 0 {
+						valuestyle += inbar
+					} else {
+						valuestyle += "fill:black"
+					}
+					datax = sx + int(dw)/2
+					canvas.Text(datax, y+barheight/2, ditem, valuestyle)
+				}
+				sx += int(dw)
+			}
+			y += barheight + gutter
+		}
+
+		// plain bars
+		for _, d := range b.Bitem {
+			canvas.Text(x-sep, y+barheight/2, d.Name, labelstyle)
+			dw := vmap(d.Value, scalemin, scalemax, 0, float64(maxwidth))
+			var barop float64
+			if b.Samebar {
+				barop = 0.3
+			} else {
+				barop = 1.0
+			}
+			if len(d.Color) > 0 {
+				canvas.Roundrect(x, y, int(dw), barheight, cr, cr, fmt.Sprintf("fill:%s;fill-opacity:%.2f", d.Color, barop))
+			} else {
+				canvas.Roundrect(x, y, int(dw), barheight, cr, cr, fmt.Sprintf("fill-opacity:%.2f", barop))
+			}
+			if showdata || b.Showdata {
+				var valuestyle = "fill-opacity:1;font-style:italic;font-size:75%;text-anchor:start;baseline-shift:-25%;"
+				var ditem string
+				var datax int
+				if len(b.Unit) > 0 {
+					ditem = fmt.Sprintf(valformat+"%s", d.Value, b.Unit)
+				} else {
+					ditem = fmt.Sprintf(valformat, d.Value)
+				}
+				if len(inbar) > 0 {
+					valuestyle += inbar
+					datax = x + fontsize/2
+				} else {
+					valuestyle += "fill:black"
+					datax = x + int(dw) + fontsize/2
+				}
+				canvas.Text(datax, y+barheight/2, ditem, valuestyle)
+			}
+			if !d.Samebar {
+				y += barheight + gutter
+			}
+		}
+		canvas.Gend()
 
 		// draw the scale and borders
-		if showgrid || b.Showgrid == "on" {
-			canvas.Line(x, chartop, x+maxwidth, y, borderstyle)                       // top border
+		chartbot := y + gutter
+		if showgrid || b.Showgrid {
+			canvas.Line(x, chartop, x+maxwidth, chartop, borderstyle)                 // top border
 			canvas.Line(x, chartbot-gutter, x+maxwidth, chartbot-gutter, borderstyle) // bottom border
 		}
 		if showscale {
@@ -165,44 +262,12 @@ func drawbc(bg Barchart, canvas *svg.SVG) {
 			for sc := scalemin; sc <= scalemax; sc += scaleincr {
 				scx := vmap(sc, scalemin, scalemax, 0, float64(maxwidth))
 				canvas.Text(x+int(scx), chartbot+fontsize, fmt.Sprintf(scfmt, sc))
-				if showgrid || b.Showgrid == "on" {
+				if showgrid || b.Showgrid {
 					canvas.Line(x+int(scx), chartbot, x+int(scx), chartop, borderstyle) // grid line
 				}
 			}
 			canvas.Gend()
 		}
-
-		// draw the data items
-		canvas.Gstyle(datastyle + color)
-		for _, d := range b.Bitem {
-			canvas.Text(x-sep, y+barheight/2, d.Name, labelstyle)
-			dw := vmap(d.Value, scalemin, scalemax, 0, float64(maxwidth))
-			if len(d.Color) > 0 {
-				canvas.Roundrect(x, y, int(dw), barheight, cr, cr, "fill:"+d.Color)
-			} else {
-				canvas.Roundrect(x, y, int(dw), barheight, cr, cr)
-			}
-			if showdata || b.Showdata == "on" {
-				var valuestyle = "font-style:italic;font-size:75%;text-anchor:start;baseline-shift:-25%;"
-				var ditem string
-				var datax int
-				if len(b.Unit) > 0 {
-					ditem = fmt.Sprintf("%v%s", d.Value, b.Unit)
-				} else {
-					ditem = fmt.Sprintf("%v", d.Value)
-				}
-				if len(inbar) > 0 {
-					valuestyle += inbar
-					datax = x + fontsize/2
-				} else {
-					valuestyle += "fill:black"
-					datax = x + int(dw) + fontsize/2
-				}
-				canvas.Text(datax, y+barheight/2, ditem, valuestyle)
-			}
-			y += barheight + gutter
-		}
-		canvas.Gend()
 
 		// apply the note if present
 		if len(b.Note) > 0 {
@@ -236,6 +301,7 @@ func vmap(value float64, low1 float64, high1 float64, low2 float64, high2 float6
 	return low2 + (high2-low2)*(value-low1)/(high1-low1)
 }
 
+// maxitem finds the maxima is a collection of bar items
 func maxitem(data []bitem) float64 {
 	max := -math.SmallestNonzeroFloat64
 	for _, d := range data {
@@ -246,6 +312,23 @@ func maxitem(data []bitem) float64 {
 	return max
 }
 
+// maxstack finds the maxima is a stack of bars
+func maxstack(stacks []bstack) float64 {
+	max := -math.SmallestNonzeroFloat64
+	for _, s := range stacks {
+		sv := stackvalues(s.Value)
+		sum := 0.0
+		for _, d := range sv {
+			sum += d
+		}
+		if sum > max {
+			max = sum
+		}
+	}
+	return max
+}
+
+// scale values returns the min, max, increment from a set of bar items
 func scalevalues(data []bitem) (float64, float64, float64) {
 	var m, max, increment float64
 	rui := 5
@@ -259,6 +342,21 @@ func scalevalues(data []bitem) (float64, float64, float64) {
 	return 0, max, increment
 }
 
+// scalestack returns the min, max, increment from a stack of bars
+func scalestack(data []bstack) (float64, float64, float64) {
+	var m, max, increment float64
+	rui := 5
+	m = maxstack(data)
+	max = roundup(m, 100)
+	if max > 2 {
+		increment = roundup(max/float64(rui), 10)
+	} else {
+		increment = 0.4
+	}
+	return 0, max, increment
+}
+
+// roundup rouds a floating point number up
 func roundup(n float64, m int) float64 {
 	i := int(n)
 	if i <= 2 {
@@ -269,10 +367,46 @@ func roundup(n float64, m int) float64 {
 	return float64(i)
 }
 
+// stack value returns the values from the value string of a stack
+func stackvalues(s string) []float64 {
+	v := strings.Split(s, "/")
+	if len(v) <= 0 {
+		return nil
+	}
+	vals := make([]float64, len(v))
+	for i, x := range v {
+		f, err := strconv.ParseFloat(x, 64)
+		if err != nil {
+			vals[i] = 0
+		} else {
+			vals[i] = f
+		}
+	}
+	return vals
+}
+
+
+// colorange evenly distributes opacity across a range of values
+func colorange(start, end float64, n int) []float64 {
+	v := make([]float64, n)
+	v[0] = start
+	v[n-1] = end
+	if n == 2 {
+		return v
+	}
+	incr := (end-start)/float64(n-1)
+	for i:=1; i < n-1; i++ {
+		v[i] = v[i-1] + incr
+	}
+	return v
+}
+
+
 // init sets up the command flags
 func init() {
 	flag.StringVar(&bgcolor, "bg", "white", "background color")
 	flag.StringVar(&barcolor, "bc", "rgb(200,200,200)", "bar color")
+	flag.StringVar(&valformat, "vfmt", "%v", "value format")
 	flag.IntVar(&width, "w", 1024, "width")
 	flag.IntVar(&height, "h", 800, "height")
 	flag.IntVar(&barheight, "bh", 20, "bar height")
