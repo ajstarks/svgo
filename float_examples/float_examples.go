bc384fa1e8490a0f35ecd413bdd1826a3098e4d7
Will Stevens <wstevens@cloudops.com>
2015-07-14 22:54:13 -0400

    fixed merge conflicts in float_examples.go

--- float_examples/float_examples.go
+++ float_examples/float_examples.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"flag"
-=======
->>>>>>> initial commit to add floating point support
 	"fmt"
 	"github.com/swill/svgo"
 	"os"
@@ -14,17 +11,12 @@ var (
 	width  = 33.333
 	height = 33.333
 	style  = fmt.Sprintf("fill:none;stroke:black;stroke-width:0.2")
-<<<<<<< HEAD
 	output = flag.String("output", "terminal", "output to 'file' or 'terminal'")
 )
 
 func main() {
 	flag.Parse()
-=======
-)
 
-func main() {
->>>>>>> initial commit to add floating point support
 	terminal := os.Stdout // just print the examples to the terminal.
 
 	fmt.Fprintln(terminal, "\nDifferent doc starts")
@@ -65,7 +57,6 @@ func main() {
 
 	// -- -- --
 
-<<<<<<< HEAD
 	var out *svg.SVG
 	if *output == "file" {
 		// Create an actual SVG file that can be viewed in a browser and verified...
@@ -83,19 +74,6 @@ func main() {
 	out.FloatDecimals = 0                                // the number of decimals our floats will have
 	out.StartviewUnitF(1024, 768, "mm", 0, 0, 1024, 768) // setup the view
 
-=======
-	// Create an actual SVG file that can be viewed in a browser and verified...
-	file, err := os.Create("float_examples.svg")
-	if err != nil {
-		panic("ERROR: Unable to create the 'float_example.svg' file...")
-	}
-	defer file.Close()
-
-	// setup
-	out := svg.New(terminal)
-	out.FloatDecimals = 0                                // the number of decimals our floats will have
-	out.StartviewUnitF(1024, 768, "mm", 0, 0, 1024, 768) // setup the view
->>>>>>> initial commit to add floating point support
 	// squares in top left
 	out.CenterRectF(15, 15, 20, 20, style)
 	out.RoundrectF(7, 7, 16, 16, 2, 2, style)
@@ -104,10 +82,7 @@ func main() {
 	out.PolygonF([]float64{10, 20, 15, 10}, []float64{20, 20, 10, 20}, style) // triangle
 	out.CircleF(15, 15, 1.5, style)
 	out.EllipseF(15, 18.25, 3.5, 1, style)
-<<<<<<< HEAD
 
-=======
->>>>>>> initial commit to add floating point support
 	// translated ellipses right of squares
 	out.TranslateF(30, 12.5)       // translate to the right of the boxes
 	out.TranslateRotateF(5, 0, 45) // translate then rotate
@@ -117,10 +92,7 @@ func main() {
 	out.EllipseF(0, 0, 2, 10, fmt.Sprintf("fill:blue;fill-opacity:0.5;stroke:black;stroke-width:0.2"))
 	out.Gend()
 	out.Gend()
-<<<<<<< HEAD
 
-=======
->>>>>>> initial commit to add floating point support
 	// rect below squares and lines
 	out.RectF(5, 27.5, 20, 5, style)
 	out.LineF(25, 26.25, 35, 26.25, fmt.Sprintf("stroke:black;stroke-width:0.2"))
@@ -136,7 +108,6 @@ func main() {
 		`stroke="black"`,
 		`stroke-width="0.2"`,
 		`marker-end="url(#dot)"`)
-<<<<<<< HEAD
 
 	// define a pattern test
 	pct := float64(3)
@@ -162,11 +133,4 @@ func main() {
 		fmt.Fprintln(terminal, "\nSVG output printed above...\n")
 	}
 	fmt.Fprintln(terminal, "Code is in the 'float_examples/float_examples.go' file.")
-=======
-	out.End()
-	fmt.Fprintln(terminal, "\n---  ---  ---")
-	fmt.Fprintln(terminal, "\nSaved: float_examples.svg")
-	fmt.Fprintln(terminal, "\nYou can review the elements of this SVG in your browser with 'Inspect Element'.")
-	fmt.Fprintln(terminal, "Creation code is at the bottom of 'float_examples.go'.")
->>>>>>> initial commit to add floating point support
 }
