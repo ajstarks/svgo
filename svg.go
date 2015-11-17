fd2151ebabde382463c8351c653151d6bf1657b8
Anthony Starks <ajstarks@gmail.com>
2015-11-16 20:35:46 -0500

    remove leading space in ns decl

--- svg.go
+++ svg.go
@@ -54,7 +54,7 @@ const (
 <svg`
 	svginitfmt = `%s width="%d%s" height="%d%s"`
 	svgns      = `
-     xmlns="http://www.w3.org/2000/svg" 
+     xmlns="http://www.w3.org/2000/svg"
      xmlns:xlink="http://www.w3.org/1999/xlink">`
 	vbfmt = `viewBox="%d %d %d %d"`
 
